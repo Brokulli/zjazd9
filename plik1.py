aed35698e87c21870733ea343d89d0c091f4688d
brokulli <respondek.patrycja@gmail.com>
2018-04-21 11:35:44 +0200

    zmiana komendy

--- plik1.py
+++ plik1.py
@@ -2,11 +2,12 @@
 
 print("Patrycja")
 
-import paramiko
+import paramiko # wczytuje paramiko
 
 x=paramiko.SSHClient()
 type(x)
-x.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-x.connect("localhost", username='tester01', password="testpati123")
-a,b,c=x.exec_command("uname -a")
+x.set_missing_host_key_policy(paramiko.AutoAddPolicy()) # by akceptowal zew.klucze
+x.connect("localhost", username='tester01', password="testpati123") # laczymy sie
+# a,b,c=x.exec_command("uname -a") # wywolywanie komendy
+a,b,c=x.exec_command("cat /etc/passwd")
 print b.read(),
